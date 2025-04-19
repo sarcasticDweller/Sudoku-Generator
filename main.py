6a56f1bef7e616714f722bedf67a2bfc8c845609
sarcasticDweller <capslocked.strife@gmail.com>
2025-04-19 13:58:49 -0400

    Gremlins exterminated by fixing ALL_SQUARES

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ numbers = {"ones": [], "twos": [], "threes": [], "fours": [], "fives": [], "sixe
 ALL_X_COORDS = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 ALL_Y_COORDS = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 ALL_SQUARES = ( # tuples containing two coordinate pairs
-    ((1, 1), (3, 3)), ((1, 4), (3, 6)), ((1, 7), (3, 9)),
-    ((3, 1), (6, 3)), ((3, 4), (6, 6)), ((3, 7), (6, 9)),
-    ((6, 1), (9, 3)), ((6, 4), (9, 6)), ((6, 7), (9, 9))
+    ((1, 1), (3, 3)), ((4, 1), (6, 3)), ((7, 1), (9, 3)),
+    ((1, 4), (3, 6)), ((4, 4), (6, 6)), ((7, 4), (9, 6)),
+    ((1, 7), (3, 9)), ((4, 7), (6, 9)), ((7, 7), (9, 9))
 )
 
 # logic
@@ -32,28 +32,29 @@ def build_coords_map(x_coords, y_coords):
 
 is_in_square = lambda coords, square: coords[0] >= square[0][0] and coords[0] <= square[1][0] and coords[1] >= square[0][1] and coords[1] <= square[1][1]
 
-def remove_coords_within_zone(coords_taken, coords_map, zone):
-    outlist = coords_map.copy()
-    if len(coords_taken) == 0:
-        return outlist # exit case
-    for coord in coords_taken:
-        if is_in_square(coord, zone):
-            # remove all coordinates in that square from the outlist
-            for c in outlist:
-                if is_in_square(c, zone):
-                    outlist.remove(c)
+def remove_coords_within_square(coords_taken, coords_map, squares): 
+    outlist = coords_map.copy() 
+    if len(coords_taken) == 0: 
+        return outlist
+    for square in squares:
+        for coord in coords_taken:
+            if is_in_square(coord, square):
+                outlist = [c for c in outlist if not is_in_square(c, square)]
     return outlist
 
-number, num_coords = random.choice(list(numbers.items()))
-while len(num_coords) < 9:
-    coords_map = build_coords_map(*get_possible_coords(num_coords))
-    final_coords_map = remove_coords_within_zone(num_coords, coords_map, ALL_SQUARES[0])
-    #print(final_coords_map)
+
+
+
+
+number, number_coords = random.choice(list(numbers.items()))
+while len(number_coords) < 9:
+    coords_map = build_coords_map(*get_possible_coords(number_coords))
+    final_coords_map = remove_coords_within_square(number_coords, coords_map, ALL_SQUARES)
     choice = random.choice(final_coords_map)
-    num_coords.append(choice)
+    number_coords.append(choice)
 
 
 # Example usage
-debug.display_grid(num_coords)
+debug.display_grid(number_coords)
 
 
